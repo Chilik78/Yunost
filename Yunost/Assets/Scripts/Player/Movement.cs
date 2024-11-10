f0423953c31bf5d0e1d60948fc84a6215630e811
Chilik78 <104494266+Chilik78@users.noreply.github.com>
2024-11-10 20:28:21 +0300

    Скрипт ходьбы, камеры ГГ

---
+++ Yunost/Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 3f;
+    public float rotationAngle = 720f;
+
+    private Rigidbody rb;   
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>(); 
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        movement.Normalize();
+
+        transform.Translate(movement * speed * Time.fixedDeltaTime, Space.World);
+
+        if (movement != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movement, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationAngle * Time.deltaTime);
+        }
+    }
+}
