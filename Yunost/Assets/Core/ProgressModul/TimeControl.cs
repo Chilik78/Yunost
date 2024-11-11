17f1241a1851a9d1bc15b963bfde6711969ebac8
b_es <ivan2003064@gmail.com>
2024-11-11 19:03:59 +0300

    Основные части модуля

---
+++ Yunost/Assets/Core/ProgressModul/TimeControl.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ProgressModul
+{
+    public class TimeControl
+    {
+        int _currentTime = 0;
+        public event Action TimeChanged;
+
+        public TimeControl(int currentTime)
+        {
+            this.currentTime = currentTime;
+        }
+
+        public int currentTime
+        {
+            get => _currentTime;
+            set
+            {
+                if (value > 0)
+                {
+                    _currentTime = value;
+                    if (TimeChanged != null)
+                    {
+                        TimeChanged();
+                    }
+                }
+            }
+        }
+    }
+}
