a1331728c6f3e4ce69f7c325c362d4d529dbbad8
b_es <ivan2003064@gmail.com>
2024-11-23 22:27:45 +0300

    Фича: добавил TaskObserver и Task

---
+++ Yunost/Assets/_Project/Develop/Core/ProgressModul/Task.cs
@@ -0,0 +1,42 @@
+﻿
+
+namespace ProgressModul
+{
+    public class Task
+    {
+        private string _id;
+        private string _name;
+        private string _description;
+        private bool _isDone = false;
+
+        public Task(string name, string description) {
+            _name = name;
+            _description = description;
+        }
+
+        public void setDone()
+        {
+            _isDone = true;
+        }
+
+        public string Id
+        {
+            get => _id;
+        }
+
+        public string Name 
+        {
+            get => _name;
+        }
+
+        public string Description
+        {
+            get => _description;
+        }
+
+        public bool isDone
+        {
+            get => _isDone;
+        }
+    }
+}
---
+++ Yunost/Assets/_Project/Develop/Core/ProgressModul/TaskObserver.cs
@@ -0,0 +1,51 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProgressModul
+{
+    public class TaskObserver
+    {
+        private List<Task> _inProgressTasks;
+        private List<Task> _doneTasks;
+        public event Action HaveDoneTask;
+
+        public TaskObserver(List<Task> initInProgressTasks, List<Task> initDoneTasks)
+        {
+            _inProgressTasks = initInProgressTasks;
+            _doneTasks = initDoneTasks;
+        }
+
+        public TaskObserver(List<Task> initInProgressTasks)
+        {
+            _inProgressTasks = initInProgressTasks;
+            _doneTasks = new();
+        }
+
+        public List<Task> GetInProgressTasks
+        {
+            get => _inProgressTasks;
+        }
+
+        public List<Task> GetDoneTasks
+        {
+            get => _doneTasks;
+        }
+
+        public void setDoneTask(Task task)
+        {
+            task.setDone();
+            _inProgressTasks.Remove(task);
+            _doneTasks.Add(task);
+            HaveDoneTask();
+        }
+
+        public Task GetLastDoneTask
+        {
+            get => _doneTasks.Last();
+        }
+    }
+}
+
+
