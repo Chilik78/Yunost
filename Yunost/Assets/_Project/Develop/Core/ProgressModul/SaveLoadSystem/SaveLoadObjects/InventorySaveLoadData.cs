77797f9b3cd25f77e391183dc10b913e8cff4c2c
b_es <ivan2003064@gmail.com>
2025-01-09 20:53:01 +0300

    Удаление камеры и пакеты

---
+++ Yunost/Assets/_Project/Develop/Core/ProgressModul/SaveLoadSystem/SaveLoadObjects/InventorySaveLoadData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace ProgressModul.Test
+{
+    /// <summary>
+    /// Custom save load data for inventory. Appends with equipped item and equipped tool.
+    /// </summary>
+    public class InventorySaveLoadData : SaveLoadData
+    {
+        public int EquippedItem { get; private set; }
+        public int EquippedArmor { get; private set; }
+
+        public InventorySaveLoadData(string id, List<InventoryItem> data, int equippedItem, int equippedArmor) : base(id, new object[] { data, equippedItem, equippedArmor })
+        {
+            EquippedItem = equippedItem;
+            EquippedArmor = equippedArmor;
+        }
+    }
+}
---
+++ Yunost/Assets/_Project/Develop/Core/ProgressModul/SaveLoadSystem/Test/Saver.cs
@@ -0,0 +1,44 @@
+﻿using EasyButtons;
+using UnityEngine;
+
+namespace ProgressModul.Test
+{
+    /// <summary>
+    /// Helper monobehaviour for saving from unity inspector.
+    /// </summary>
+    public class Saver : MonoBehaviour
+    {
+        [SerializeField] private InventoryHolder _inventoryHolder;
+        TaskObserver taskObserver;
+
+        private SaveLoadSystem _saveLoadSystem;
+
+        private void Start()
+        {
+            _saveLoadSystem ??= new();
+            TextAsset initTasksJson = Resources.Load<TextAsset>("InitTasks");
+            taskObserver = new(initTasksJson.text);
+            _saveLoadSystem.AddToSaveLoad(_inventoryHolder.Inventory);
+            _saveLoadSystem.AddToSaveLoad(taskObserver);
+        }
+
+        [Button("Save to file")]
+        private void Save()
+        {
+            _saveLoadSystem.SaveGame(SaveType.File);
+        }
+
+        [Button("Load from file")]
+        private void Load()
+        {
+            _saveLoadSystem.LoadGame(SaveType.File);
+            _inventoryHolder.UpdateItems();
+            foreach (var task in taskObserver.GetInProgressTasks)
+            {
+                Debug.Log(task.Name);
+                foreach(var subTask in task.GetInProgressSubTasks) Debug.Log(subTask.Description);
+            } 
+            foreach (var task in taskObserver.GetDoneTasks) Debug.Log(task.Description);
+        }
+    }
+}
