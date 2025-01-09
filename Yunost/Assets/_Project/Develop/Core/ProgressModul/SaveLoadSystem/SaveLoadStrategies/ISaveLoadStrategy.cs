5783a38823710a550691196a9a5b92adc4abca19
b_es <ivan2003064@gmail.com>
2025-01-09 18:28:54 +0300

    Добавил систему загрузки/сохранений

---
+++ Yunost/Assets/_Project/Develop/Core/ProgressModul/SaveLoadSystem/SaveLoadStrategies/ISaveLoadStrategy.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+
+namespace ProgressModul
+{
+    /// <summary>
+    /// Base interface for save load strategies.
+    /// </summary>
+    public interface ISaveLoadStrategy
+    {
+        /// <summary>
+        /// Save list of objects.
+        /// </summary>
+        /// <param name="objectsToSave">Array of objects to be saved.</param>
+        public void Save(IEnumerable<ISaveLoadObject> objectsToSave);
+
+        /// <summary>
+        /// Load data.
+        /// </summary>
+        /// <returns>Loaded data.</returns>
+        public SaveLoadData[] Load();
+    }
+}
