020d75ec6ec8c67c3773535207aea63b9cb042e7
b_es <ivan2003064@gmail.com>
2025-01-22 22:16:44 +0300

    Подключил TaksObserver к дневнику

--- Yunost/Assets/_Project/Develop/Scripts/Diary/DiaryManager.cs
+++ Yunost/Assets/_Project/Develop/Scripts/Diary/DiaryManager.cs
@@ -2,6 +2,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using Global;
+using ProgressModul;
+using System.Linq;
 
 public class DiaryManager : MonoBehaviour
 {
@@ -45,16 +48,32 @@ public class DiaryManager : MonoBehaviour
         }
 
 
-        //для тестов
+        /*//для тестов
         string mainTask = "Найти Шнюка";
         string[] subTasks = {
             "Поговорить с Пупсенем",
             "Обыскать хату Вупсеня",
             "Найти клад бабки",
             "Вернуться на луну"
-        };
+        };*/
 
-        //FindObjectOfType<DiaryManager>().UpdateDiary(mainTask, subTasks);
+        var taskObserver = ServiceLocator.Get<TaskObserver>();
+
+        var task = taskObserver.GetFirstInProgressTask;
+        UpdateTask(task);
+
+        taskObserver.HaveNewTask += UpdateTask;
+        taskObserver.HaveNewSubTask += UpdateTask;
+    }
+
+    private void UpdateTask(Task task)
+    {
+        if (task == null)
+        {
+            Debug.LogWarning("Задания в прогрессе исчерпаны");
+            return;
+        }
+        UpdateDiary(task.Name, task.GetInProgressSubTasks.Select(x => x.Description).ToArray());
     }
 
     private void Update()
